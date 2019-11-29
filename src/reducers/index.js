01837d96efe823d09cced0e91d8c2354a9c0098d
Maksym <wolfsangel009@gmail.com>
2019-11-29 15:51:07 +0200

    Edited the cart, added the ability to increase and decrease the number of products, as well as remove profucts from the cart.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,6 +8,13 @@ const initialState = {
 
 const updateCartItems = (cartItems, item, idx) => {
 
+   if (item.count === 0) {
+      return [
+         ...cartItems.slice(0, idx),
+      ...cartItems.slice(idx + 1)
+      ];
+   }
+
    if (idx === -1) {
       return [
          ...cartItems,
@@ -22,7 +29,7 @@ const updateCartItems = (cartItems, item, idx) => {
    ];
 };
 
-const updateCartItem = (book, item = {}) => {
+const updateCartItem = (book, item = {}, quantity) => {
 
    const {
       id = book.id,
@@ -33,9 +40,23 @@ const updateCartItem = (book, item = {}) => {
    return {
       id,
       title,
-      count: count + 1,
-      total: total + book.price
+      count: count + quantity,
+      total: total + quantity*book.price
    };
+};
+
+const updateOrder = (state, bookId, quantity) => {
+   const { books, cartItems } = state;
+
+         const book = books.find(({ id }) => id === bookId);
+         const itemIndex = cartItems.findIndex(({ id }) => id === bookId);
+         const item = cartItems[itemIndex];
+
+         const newItem = updateCartItem(book, item, quantity);
+         return {
+            ...state,
+            cartItems: updateCartItems(cartItems, newItem, itemIndex)
+         };
 };
 
 const reducer = (state = initialState, action) => {
@@ -66,16 +87,14 @@ const reducer = (state = initialState, action) => {
          };
 
       case 'BOOK_ADDED_TO_CART':
-         const bookId = action.payload;
-         const book = state.books.find((book) => book.id === bookId);
-         const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
-         const item = state.cartItems[itemIndex];
+         return updateOrder(state, action.payload, 1);
 
-         const newItem = updateCartItem(book, item);
-         return {
-            ...state,
-            cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
-         };
+      case 'BOOK_REMOVED_FROM_CART':
+            return updateOrder(state, action.payload, -1);
+
+      case 'ALL_BOOKS_REMOVED_FROM_CART':
+         const item = state.cartItems.find(({ id }) => id === action.payload);
+         return updateOrder(state, action.payload, -item.count);
 
       default:
          return state;
